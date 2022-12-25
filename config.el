e746d24dd33fe3b5dabd5a56d02e6f52753b89e1
Jom Dollesin <codewithjom@gmail.com>
2022-12-25 17:20:50 +0800

    fix(doom): adding configurations to my vanilla emacs.

--- config.el
+++ config.el
@@ -1,6 +1,19 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+(setq backup-directory-alist `(("." . ,(expand-file-name "tmp/backups/" user-emacs-directory))))
+
+(make-directory (expand-file-name "tmp/auto-saves/" user-emacs-directory) t)
+
+(setq auto-save-list-file-prefix
+      (expand-file-name "tmp/auto-saves/sessions/"
+                        user-emacs-directory)
+      auto-save-file-name-transforms
+      `((".*" ,(expand-file-name "tmp/auto-saves/"
+                                 user-emacs-directory) t)))
+
+(set-default-coding-systems 'utf-8)
+
 (setq inhibit-startup-message t)
 (scroll-bar-mode -1)        ; Disable visible scrollbar
 (tool-bar-mode -1)          ; Disable the toolbar
@@ -10,6 +23,27 @@
 (setq visible-bell t)
 (blink-cursor-mode 1)
 ;; (global-hl-line-mode t)
+(setq-default tab-width 2)
+
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
+(setq use-dialog-box nil) ;; Disable dialog boxes since they weren't working in Mac OSX
+
+(set-frame-parameter (selected-frame) 'alpha '(95 . 90))
+(add-to-list 'default-frame-alist '(alpha . (95 . 90)))
+(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+(setq display-line-numbers-type t)
+
+(dolist (mode '(org-mode-hook
+                vterm-mode-hook
+                markdown-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 (use-package dashboard
   :init      ;; tweak dashboard config before loading it
@@ -53,43 +87,269 @@ List of keybindings (SPC h b b)")
   '(font-lock-comment-face :slant italic)
   '(font-lock-keyword-face :slant italic))
 
-(setq display-line-numbers-type t)
-
-(custom-set-faces
- '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold :family "variable-pitch"))))
- '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.7))))
- '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.6))))
- '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.5))))
- '(markdown-header-face-4 ((t (:inherit markdown-header-face :height 1.4))))
- '(markdown-header-face-5 ((t (:inherit markdown-header-face :height 1.3))))
- '(markdown-header-face-6 ((t (:inherit markdown-header-face :height 1.2)))))
-
 (map! :leader
       :desc "Org babel tangle" "m B" #'org-babel-tangle)
 
-(after! org
-  (setq org-directory "~/Documents/org"
-        org-agenda-files '("~/Documents/org/agenda.org")
-        org-default-notes-file (expand-file-name "notes.org" org-directory)
-        org-ellipsis " ▼ "
-        org-superstar-headline-bullets-list '("◉" "●" "○" "◆" "●" "○" "◆")
-        org-superstar-itembullet-alist '((?+ . ?➤) (?- . ?✦))
-        org-log-done 'time
-        org-hide-emphasis-markers t))
+(setq-default fill-column 80)
+(use-package simple-httpd
+  :ensure t)
 
-;; (defun jd/org-colors ()
-;;   (interactive)
-;;   (dolist
-;;       (face
-;;        '((org-level-1 1.7 "#268bd2" ultra-bold)
-;;          (org-level-2 1.6 "#d33682" extra-bold)
-;;          (org-level-3 1.5 "#859900" bold)
-;;          (org-level-4 1.4 "#b58900" semi-bold)
-;;          (org-level-5 1.3 "#cb4b16" normal)
-;;          (org-level-6 1.3 "#6c71c4" normal)
-;;          (org-level-7 1.3 "#2aa198" normal)
-;;          (org-level-8 1.3 "#657b83" normal)))
-;;     (set-face-attribute (nth 0 face) nil :font doom-variable-pitch-font :weigth (nth 3 face) :height (nth 1 face) :foreground (nth 2 face))
-;;     (set-face-attribute 'org-table nil :font doom-font :weight 'normal :height 1.0 :foreground "#bfafdf")))
-;;
-;; (jd/org-colors)
+(setq org-image-actual-width nil)
+
+(defun jd/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+  (setq evil-auto-indent nil))
+
+(use-package org
+  :defer t
+  :hook (org-mode . jd/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▼"
+        org-hide-emphasis-markers t
+        org-src-fontify-natively t
+        org-fontify-quote-and-verse-blocks t
+        org-src-tab-acts-natively t
+        org-edit-src-content-indentation 2
+        org-hide-block-startup nil
+        org-src-preserve-indentation nil
+        org-startup-folded 'content
+        org-cycle-separator-lines 2)
+
+  (setq or-modules '(org-crypt
+                     org-habit
+                     org-bookmark
+                     org-eshell
+                     org-irc))
+  (setq org-refile-targets '((nil :maxlevel . 1)
+                             (org-agenda-files :maxlevel . 1)))
+
+  (setq org-outline-path-complete-in-steps nil)
+  (setq org-refiles-use-outline-path t)
+
+  (evil-define-key '(normal insert visual) org-mode-map (kbd "C-j") 'org-next-visible-heading)
+  (evil-define-key '(normal insert visual) org-mode-map (kbd "C-k") 'org-previous-visible-heading)
+
+  (evil-define-key '(normal insert visual) org-mode-map (kbd "M-j") 'org-metadown)
+  (evil-define-key '(normal insert visual) org-mode-map (kbd "M-k") 'org-metaup)
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)))
+
+  (push '("conf-unix" . conf-unix) org-src-lang-modes)
+
+  (use-package org-superstar
+    :after org
+    :hook (org-mode . org-superstar-mode)
+    :custom
+    (org-directory "~/Documents/orgmode/")
+    (org-superstar-remove-leading-stars t)
+    ;; (org-ellipsis "...")
+    (org-superstar-item-bullet-alist '((?+ . ?✦) (?- . ?–)))
+    (org-superstar-headline-bullets-list '("◉" "●" "○" "●" "○" "●" "○")))
+
+  ;; Increase the size of various headings
+  (set-face-attribute 'org-document-title nil :font "JetBrains Mono Nerd Font" :weight 'bold :height 1.3)
+  (dolist (face '((org-level-1 . 1.3)
+                  (org-level-2 . 1.2)
+                  (org-level-3 . 1.1)
+                  (org-level-4 . 1.05)
+                  (org-level-5 . 1.05)
+                  (org-level-6 . 1.05)
+                  (org-level-7 . 1.05)
+                  (org-level-8 . 1.05)))
+  (set-face-attribute (car face) nil :font "JetBrains Mono Nerd Font" :weight 'bold :height (cdr face)))
+
+  (require 'org-indent)
+
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  ;; (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+
+  ;; Get rid of the background on column views
+  (set-face-attribute 'org-column nil :background nil)
+  (set-face-attribute 'org-column-title nil :background nil))
+
+(use-package org-roam
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "~/Documents/orgmode/")
+  (org-roam-completion-everywhere t)
+  (org-roam-capture-templates
+   '(("d" "default" plain
+     "%?"
+     :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}" "#+title: ${title}\n")
+     :unnarrowed t)
+     ("l" "programming language" plain
+      "* Get Started\n\n- Topic: %?\n- Language: \n\n"
+      :if-new (file+head "${slug}.org" "#+title: ${title}\n")
+      :unnarrowed t)
+     ("b" "book notes" plain
+      "\n* Source\n\nAuthor: %^{Author}\nTitle: ${title}\n\n"
+      :if-new (file+head "${slug}.org" "#+title: ${title}\n")
+      :unnarrowed t)
+     ("p" "project" plain "* Goals\n\n%?\n\n* Tasks\n\n** TODO Add initial tasks\n\n* Dates\n\n"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+filetags: Project")
+      :unnarrowed t)))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert))
+  :config
+  (org-roam-setup))
+
+(require 'org-tempo)
+
+(add-to-list 'org-structure-template-alist '("src" . "src"))
+(add-to-list 'org-structure-template-alist '("sh" . "src sh"))
+(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+(add-to-list 'org-structure-template-alist '("java" . "src java"))
+
+(use-package org-appear
+  :hook (org-mode . org-appear-mode))
+
+(use-package lsp-mode
+  :ensure t
+  :commands lsp
+  :hook (lsp-mode . lsp-enable-which-key-integration)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  (setq lsp-completion-enable-additional-text-edit nil))
+
+(use-package lsp-ui
+  :ensure t
+  :hook (lsp-mode . lsp-ui-mode)
+  :config
+  (setq lsp-ui-sideline-enable t)
+  (setq lsp-ui-sideline-show-hover t)
+  (setq lsp-ui-doc-position 'bottom))
+
+(use-package dap-mode
+  :after lsp-mode
+  :ensure t
+  :custom
+  (lsp-enable-dap-auto-configure nil)
+  :config
+  (dap-auto-configure-mode)
+  (dap-ui-mode 1)
+  (dap-tooltip-mode 1)
+  (require 'dap-node)
+  (dap-node-setup))
+
+(use-package lispy
+  :hook ((emacs-lisp-mode . lispy-mode)
+         (scheme-mode . lispy-mode)))
+
+;; (use-package evil-lispy
+;;   :hook ((lispy-mode . evil-lispy-mode)))
+
+(use-package lispyville
+  :hook ((lispy-mode . lispyville-mode))
+  :config
+  (lispyville-set-key-theme '(operators c-w additional
+                              additional-movement slurp/barf-cp
+                              prettify)))
+
+(use-package cider
+  :mode "\\.clj[sc]?\\'"
+  :config
+  (evil-collection-cider-setup))
+
+(use-package nvm
+  :defer t)
+
+(use-package typescript-mode
+  :mode "\\.ts\\'"
+  :config
+  (setq typescript-indent-level 2))
+
+(defun jd/set-js-indentation ()
+  (setq js-indent-level 2)
+  (setq evil-shift-width js-indent-level)
+  (setq-default tab-width 2))
+
+(use-package js2-mode
+  :mode "\\.jsx?\\'"
+  :config
+  ;; Use js2-mode for Node scripts
+  (add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
+
+  ;; Don't use built-in syntax checking
+  (setq js2-mode-show-strict-warnings nil)
+
+  ;; Set up proper indentation in JavaScript and JSON files
+  (add-hook 'js2-mode-hook #'jd/set-js-indentation)
+  (add-hook 'json-mode-hook #'jd/set-js-indentation))
+
+(use-package prettier-js
+:hook ((js2-mode . prettier-js-mode)
+       (typescript-mode . prettier-js-mode))
+:config
+(setq prettier-js-show-errors nil))
+
+(use-package markdown-mode
+  :mode "\\.md\\'"
+  :config
+  (setq markdown-command "marked")
+  (defun jd/set-markdown-header-font-sizes ()
+    (dolist (face '((markdown-header-face-1 . 1.2)
+                    (markdown-header-face-2 . 1.1)
+                    (markdown-header-face-3 . 1.0)
+                    (markdown-header-face-4 . 1.0)
+                    (markdown-header-face-5 . 1.0)))
+      (set-face-attribute (car face) nil :weight 'normal :height (cdr face))))
+
+  (defun jd/markdown-mode-hook ()
+    (jd/set-markdown-header-font-sizes))
+
+  (add-hook 'markdown-mode-hook 'jd/markdown-mode-hook))
+
+;; Convert org file to markdown
+(eval-after-load "org"
+  '(require 'ox-md nil t))
+
+(use-package web-mode
+  :mode "(\\.\\(html?\\|ejs\\|tsx\\|jsx\\)\\'"
+  :config
+  (setq-default web-mode-code-indent-offset 2)
+  (setq-default web-mode-markup-indent-offset 2)
+  (setq-default web-mode-attribute-indent-offset 2))
+
+(use-package skewer-mode)
+(use-package prettier
+  :config
+  (prettier-mode t))
+
+(use-package compile
+  :custom
+  (compilation-scroll-output t))
+
+(defun auto-recompile-buffer ()
+  (interactive)
+  (if (member #'recompile after-save-hook)
+      (remove-hook 'after-save-hook #'recompile t)
+    (add-hook 'after-save-hook #'recompile nil t)))
+
+(use-package flycheck
+  :defer t
+  :hook (lsp-mode . flycheck-mode))
+
+(use-package yasnippet
+  :hook (prog-mode . yas-minor-mode)
+  :config
+  (setq yas-snippet-dirs '("~/Repo/snippets"))
+  (yas-global-mode 1))
+
+(use-package smartparens
+  :hook (prog-mode . smartparens-mode))
