66413fbfe0ec0cd1ddc740ae7472c23167f93bc5
Jom Dollesin <codewithjom@gmail.com>
2023-02-10 16:37:28 +0800

    fix(doom): disable transparency.

--- config.el
+++ config.el
@@ -1,5 +1,5 @@
-(set-frame-parameter (selected-frame) 'alpha '(90 . 90))
-(add-to-list 'default-frame-alist '(alpha . (90 . 90)))
+;; (set-frame-parameter (selected-frame) 'alpha '(90 . 90))
+;; (add-to-list 'default-frame-alist '(alpha . (90 . 90)))
 
 (add-to-list 'default-frame-alist '(width  . 80))
 (add-to-list 'default-frame-alist '(height . 32))
